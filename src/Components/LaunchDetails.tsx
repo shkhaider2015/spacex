a6b6df60a3c79b7b16a7551761c4cb636e168d9c
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2021-07-26 02:46:04 +0500

    add carousel

--- src/Components/LaunchDetails.tsx
+++ src/Components/LaunchDetails.tsx
@@ -3,21 +3,20 @@ import { useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom"
 import { LAUNCHES } from "../Query/Queries";
 import { ILaunches } from '../Types/queryTypes';
+import { Carousel } from "react-bootstrap";
 
 
 
-export const LaunchDetails = () =>
-{
+export const LaunchDetails = () => {
     const { launchid } = useParams();
-    const {loading, error, data} = useQuery(LAUNCHES);
+    const { loading, error, data } = useQuery(LAUNCHES);
     const [details, setDetails] = React.useState<ILaunches>();
-    
+
     React.useEffect(
-    () => {
+        () => {
             data.launches.map(
-                (item:ILaunches) => {
-                    if(item.id === launchid)
-                    {
+                (item: ILaunches) => {
+                    if (item.id === launchid) {
                         setDetails(item);
                     }
                     return null
@@ -27,19 +26,52 @@ export const LaunchDetails = () =>
         }, [data]
     )
 
-    if(loading)
-    {
+    if (loading) {
         return <div> ...Loading </div>
     }
-    if(error)
-    {
+    if (error) {
         return <div> Error </div>
     }
-    
 
-    return <div>
-        {
-            details?.id
-        }
+
+    return <div className="row" >
+        <div className="col-lg-12  border" >
+            <div className="d-flex flex-row justify-content-center" >
+                <span className="display-1 p-5" >{details?.mission_name}</span>
+                <div className="d-flex flex-column p-5 pb-0 display-10" >
+                    <span> {details?.launch_year} </span>
+                    <span className={details?.launch_success ? "text-success" : "text-danger"} > 
+                    {details?.launch_success ? "Successful" : "Failed"} 
+                    </span>
+                </div>
+            </div>
+        </div>
+
+        <div className="row border mt-2" >
+            <div className="col-3" ></div>
+            <div className="col-6 text-center " >
+                <p className="lead" > {details?.details} </p> 
+            </div>
+            <div className="col-3" ></div>
+        </div>
+
+        <div className="row mt-3 pb-5" >
+            <span className="col-lg-2" ></span>
+            <Carousel className="col-lg-8 pl-0 border" >
+                {
+                    details?.links.flickr_images.map(
+                        (image:string, index:number) => <Carousel.Item key={index} interval={1000}>
+                        <img
+                            className="d-block w-100"
+                            src={image}
+                            alt={`Slide No ${index}`}
+                            height="500px"
+                        />
+                    </Carousel.Item>
+                    )
+                }
+            </Carousel>
+            <span className="col-lg-2" ></span>
+        </div>
     </div>
 }
